9bcea9612970a6cce21443973c628bdd9c83008a
svud90 <svud90@gmail.com>
2017-02-12 22:13:02 +1000

    dependency injection

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -7,26 +7,52 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ExploreCalifornia
 {
     public class Startup
     {
+        private readonly IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            configuration = new ConfigurationBuilder()
+                           .AddEnvironmentVariables()
+                           .AddJsonFile(env.ContentRootPath + "/config.json")
+                           .AddJsonFile(env.ContentRootPath + "/config.development.json", true).Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,FeatureToggles featureToggles)
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+            var configuration = new ConfigurationBuilder()
+                                    .AddEnvironmentVariables()
+                                    .AddJsonFile(env.ContentRootPath+"/config.json")
+                                    .AddJsonFile(env.ContentRootPath + "/config.development.json",true).Build();
+                                    
+            if (featureToggles.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (AppContext, next) =>
+            {
+                if (AppContext.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+                await next();
+            });
             app.UseFileServer();
         }
     }
