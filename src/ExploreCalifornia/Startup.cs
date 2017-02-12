c2aa6f07ffbaac10eaff72bb7cbbfb0f9398d44b
svud90 <svud90@gmail.com>
2017-02-12 23:41:40 +1000

    inject helper

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using ExploreCalifornia.Models;
 
 namespace ExploreCalifornia
 {
@@ -26,6 +27,7 @@ namespace ExploreCalifornia
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FormattingService>();
             services.AddTransient<FeatureToggles>(x => new FeatureToggles {
                 EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
             });
